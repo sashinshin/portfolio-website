de59a4ce8de21a20e5018f22103a0e26cbbe3372
Sasha <83590472+sashinshin@users.noreply.github.com>
2021-09-01 15:36:18 +0200

    Add files via upload

---
+++ gatsby-config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  siteMetadata: {
+    siteUrl: "https://www.yourdomain.tld",
+    title: "portfolio",
+  },
+  plugins: [
+    "gatsby-plugin-netlify-cms",
+    "gatsby-plugin-sass",
+    "gatsby-plugin-gatsby-cloud",
+  ],
+};
