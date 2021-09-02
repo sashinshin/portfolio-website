a3fc5b8e444c1d821abd713af11cb0916e732fc8
Sasha <sasha.luhr@gmail.com>
2021-09-02 17:57:46 +0200

    Fixed egeometry exports

--- src/index/threejs/geometry.js
+++ src/index/threejs/geometry.js
@@ -2,15 +2,17 @@ import * as THREE from 'three';
 import moonImage from '../images/moon.jpg';
 import normalImage from '../images/normal.jpg'
 
+let torus, addStar, moon;
+
 if (typeof document !== undefined) {
     // Create torus
     const torusGeometry = new THREE.TorusGeometry(10, 3, 16, 100);
     //const material = new THREE.MeshBasicMaterial( {color: 0xFF6347, wireframe: true});
     const torusMaterial = new THREE.MeshPhongMaterial( {color: 0xFF6347});
-    const torus = new THREE.Mesh(torusGeometry, torusMaterial);
+    torus = new THREE.Mesh(torusGeometry, torusMaterial);
     
     // Random stars
-    const addStar = (scene) => {
+    addStar = (scene) => {
         const geometry = new THREE.SphereGeometry(0.25, 24, 24);
         const material = new THREE.MeshBasicMaterial({color: 0xffffff});
         const star = new THREE.Mesh(geometry, material);
@@ -24,14 +26,14 @@ if (typeof document !== undefined) {
     const moonTexture = new THREE.TextureLoader().load(moonImage);
     const normalTexture = new THREE.TextureLoader().load(normalImage);
     
-    const moon = new THREE.Mesh(
+    moon = new THREE.Mesh(
     new THREE.SphereGeometry(3, 32, 32),
     new THREE.MeshStandardMaterial({
         map: moonTexture,
         normalMap: normalTexture,
         })
     );
-}
 
+}
 
 export { torus, addStar, moon };
